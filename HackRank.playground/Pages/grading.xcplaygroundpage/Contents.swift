d9ef3e42df438aa9be9581a1d7e9d36150314e7f
Elijah Ponkratenko <i.ponkratenko@hh.ru>
2017-08-21 00:14:02 +0300

    grade

--- HackRank.playground/Pages/grading.xcplaygroundpage/Contents.swift
+++ HackRank.playground/Pages/grading.xcplaygroundpage/Contents.swift
@@ -8,31 +8,12 @@ let input = FileParser().parseFile(name: "gradling", ext: "")!
 let arr = input.components(separatedBy: CharacterSet.newlines).flatMap{ Int($0) }
 print(arr)
 
-//func roundToFive(x : Double) -> Int {
-//    return 5 * Int(round(x / 5))
-//}
+for grade in arr {
+    print(grade < 38 || grade % 5 < 3 ? grade : grade + (5 - (grade % 5)))
+}
 
-print( Int(round(1 / 5)) )
 
-print(arr.filter { (grade) -> Bool in
-    guard grade > 38 else {
-        return false
-    }
-    return true
-})
-print(arr)
 
-//for var grade in arr {
-//    guard grade >= 38 else {
-//        continue
-//    }
-//    if grade % 5 == 3 {
-//        grade += 2
-//    } else if grade % 5 == 4 {
-//        grade += 1
-//    }
-//    print(grade)
-//}
 
 
 
